108a4f18060524f2909bfb75916d2359bf853c25
Jessica-G-arcia <jeh.garcia.jg@gmail.com>
2024-11-11 09:29:55 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para abrir o diálogo
+function showDialog(dialogId) {
+    document.getElementById(dialogId).showModal();
+  }
+
+  // Função para fechar o diálogo
+  function closeDialog(dialogId) {
+    document.getElementById(dialogId).close();
+  }
